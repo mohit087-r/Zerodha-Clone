b4ff1e9b7c73298023db6598ab08d6626e605470
Mohit Raut <mohitraut087@gmail.com>
2025-05-11 11:47:40 +0530

    pricing-component

--- src/landing_page/pricing/PricingPage.jsx
+++ src/landing_page/pricing/PricingPage.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 
-import Hero from './Hero'
-import Brokerage from './Brokerage'
-
-
 const PricingPage = () => {
   return (
-    <div>
-      <Hero/>
-      <Brokerage/>
+    <div className='text-center mt-20'>
+      <h1 className='text-4xl font-semibold text-[#424242]'>Charges</h1>
+      <p className='text-xl text-[#424242]'>List of all charges and taxes</p>
+
+      <div className='mt-40 md:flex md:mx-30 mx-10 gap-10'>
+        <div>
+          <img className='h-40 place-self-center' src="/images/pricing0.svg"/>
+          <h2 className='text-2xl font-semibold text-[#424242]'>Free equity delivery</h2>
+          <p className='text-[#424242]'>All equity delivery investments &#40;NSE, BSE&#41;, are absolutely free — ₹ 0 brokerage.</p>
+        </div>
+        <div>
+          <img className='h-40 place-self-center' src="/images/intradayTrades.svg" alt="" />
+          <h2 className='text-2xl font-semibold text-[#424242]'>Intraday and F&O trades</h2>
+          <p className='text-[#424242]'>Flat ₹ 20 or 0.03% &#40;whichever is lower&#41; per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+        </div>
+        <div>
+          <img className='h-40 place-self-center' src="/images/pricing0.svg"/>
+          <h2 className='text-2xl font-semibold text-[#424242]'>Free direct MF</h2>
+          <p className='text-[#424242]'>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+        </div>
+      </div>
     </div>
   )
 }
